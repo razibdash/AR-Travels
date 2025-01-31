2e60080db93c0cca623c61525b42a023866cf8e4
avronilrajib <avronilrajib@gmail.com>
2025-01-31 22:13:27 +0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl bg-amber-700 text-center py-1.5 text-sky-50">
+          Hello AB Travels
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
