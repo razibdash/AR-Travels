1ddd2ff0c0de6cf6533dad03eb37dc4e27d8ac05
MughniRayhan <tishtisha13@gmail.com>
2025-05-29 21:09:06 +0600

    enhance UserProtectedWrapper to fetch user profile and manage loading state

--- frontend/src/components/ProtectedWrapper/UserProtectedWrapper.jsx
+++ frontend/src/components/ProtectedWrapper/UserProtectedWrapper.jsx
@@ -1,15 +1,44 @@
-import { useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../../context/UserContext";
+import axios from "axios";
 // eslint-disable-next-line react/prop-types
 const UserProtectedWrapper = ({ children }) => {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
+  const {user, setUser} = useContext(UserDataContext);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     if (!token) {
       // Redirect to login or show an error message
       navigate("/user-login");
+       return;
     }
-  }, [token]);
+
+    // Fetch user profile
+    axios
+      .get("http://localhost:4000/users/profile", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          setUser(res.data.user);
+          setIsLoading(false);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        localStorage.removeItem("token");
+        navigate("/user-login");
+      });
+  }, [navigate, setUser, token]);
+
+if(isLoading){
+  return <div>...Loading</div>
+}
+
   // If token exists, render the children components
   return <>{children}</>;
 };
